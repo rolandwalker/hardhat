a4c31cb609a0e055ed5408e73a9f3dff580fb547
Roland Walker <walker@pobox.com>
2012-11-26 12:28:21 -0500

    improve compat of called-interactively-p macro including trunk (to be 24.3) and versions with only `interactive-p`

--- hardhat.el
+++ hardhat.el
@@ -529,9 +529,15 @@ Set this value to nil to disable."
 
 Optional KIND is as documented at `called-interactively-p'
 in GNU Emacs 24.1 or higher."
-  (if (eq 0 (cdr (subr-arity (symbol-function 'called-interactively-p))))
-      '(called-interactively-p)
-    `(called-interactively-p ,kind)))
+  (cond
+    ((not (fboundp 'called-interactively-p))
+     '(interactive-p))
+    ((condition-case nil
+         (progn (called-interactively-p 'any) t)
+       (error nil))
+     `(called-interactively-p ,kind))
+    (t
+     '(called-interactively-p))))
 
 ;;; utility functions
 
