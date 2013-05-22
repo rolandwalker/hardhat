a2a40207760461d84eacbe6f68f47d42247d9b8b
Roland Walker <walker@pobox.com>
2013-05-22 12:43:51 -0400

    bump version

--- hardhat.el
+++ hardhat.el
@@ -5,8 +5,8 @@
 ;; Author: Roland Walker <walker@pobox.com>
 ;; Homepage: http://github.com/rolandwalker/hardhat
 ;; URL: http://raw.github.com/rolandwalker/hardhat/master/hardhat.el
-;; Version: 0.3.8
-;; Last-Updated: 18 Oct 2012
+;; Version: 0.4.0
+;; Last-Updated: 22 May 2013
 ;; EmacsWiki: Hardhat
 ;; Package-Requires: ((ignoramus "0.6.2"))
 ;; Keywords: convenience
@@ -183,7 +183,7 @@ SYMBOL and VALUE are passed to `custom-set-default'."
 ;;;###autoload
 (defgroup hardhat nil
   "Protect against clobbering user-writable files."
-  :version "0.3.8"
+  :version "0.4.0"
   :link '(emacs-commentary-link :tag "Commentary" "hardhat")
   :link '(url-link :tag "Github" "http://github.com/rolandwalker/hardhat")
   :link '(url-link :tag "EmacsWiki" "http://emacswiki.org/emacs/Hardhat")
