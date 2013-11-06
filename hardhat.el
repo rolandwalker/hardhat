a5df2e3c7b53d5955cb15309679b984acfc1779e
Roland Walker <walker@pobox.com>
2013-11-06 10:06:41 -0500

    update/stdize copyrights

--- hardhat.el
+++ hardhat.el
@@ -1,6 +1,6 @@
 ;;; hardhat.el --- Protect against clobbering user-writable files
 ;;
-;; Copyright (c) 2012 Roland Walker
+;; Copyright (c) 2012-13 Roland Walker
 ;;
 ;; Author: Roland Walker <walker@pobox.com>
 ;; Homepage: http://github.com/rolandwalker/hardhat
