rolandwalker/hardhat

commit 4051eded3ea2f69ba8f7a9c6bf181405f4366ca4

Support commit messages in Git submodules
In recent Git versions, the .git directories of submodules are not
stored in the submodule itself, but rather in a subdirectory of the .git
directory of the parent directory, e.g. the submodule foo/bar has its
.git directory in .git/modules/foo/bar/ of the parent repository.

Change the regular expression for editable Git files to allow for
arbitrary nesting, such that commit messages in submodules are
considered editable as well.

hardhat.el
@@ -430,7 +430,7 @@ All patterns are case-insensitive."
 ;; todo @@@ more editable exceptions needed here
 (defcustom hardhat-fullpath-editable-regexps '(
                                                "~/\\.cpan/CPAN/MyConfig\\.pm\\'"
-                                               "/\\.git/\\(?:COMMIT_EDITMSG\\|MERGE_MSG\\|SQUASH_MSG\\|TAG_EDITMSG\\|rebase-merge/git-rebase-todo\\|description\\|hooks/\\|config\\|GHI_ISSUE\\)\\'"
+                                               "/\\.git/.*/\\(?:COMMIT_EDITMSG\\|MERGE_MSG\\|SQUASH_MSG\\|TAG_EDITMSG\\|rebase-merge/git-rebase-todo\\|description\\|hooks/\\|config\\|GHI_ISSUE\\)\\'"
                                                ;; "~/\\.cabal/"
                                                ;; "~/perl5/perlbrew/"
                                                ;; "~/\\.npm/"

commit e5f8a1ee25316096e8a2bcfdcb4e3c04c4323fd7

Fix Git support
Fix a regression from 4051ede, which introduced submodule support by
allowing for arbitrary nesting.  Unfortunately the regexp introduced by
that commit had two consecutive path separators which broke standard Git
commit messages.

This commit makes the nested path really optional, and so fixes commit
messages in standard repositories.

Sorry for the breakage

hardhat.el
@@ -430,7 +430,7 @@ All patterns are case-insensitive."
 ;; todo @@@ more editable exceptions needed here
 (defcustom hardhat-fullpath-editable-regexps '(
                                                "~/\\.cpan/CPAN/MyConfig\\.pm\\'"
-                                               "/\\.git/.*/\\(?:COMMIT_EDITMSG\\|MERGE_MSG\\|SQUASH_MSG\\|TAG_EDITMSG\\|rebase-merge/git-rebase-todo\\|description\\|hooks/\\|config\\|GHI_ISSUE\\)\\'"
+                                               "/\\.git/\\(?:.+/\\)?\\(?:COMMIT_EDITMSG\\|MERGE_MSG\\|SQUASH_MSG\\|TAG_EDITMSG\\|rebase-merge/git-rebase-todo\\|description\\|hooks/\\|config\\|GHI_ISSUE\\)\\'"
                                                ;; "~/\\.cabal/"
                                                ;; "~/perl5/perlbrew/"
                                                ;; "~/\\.npm/"